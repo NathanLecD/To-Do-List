88316eb354c82f77929352466ad1cf127ef42430
NathanLecD <nathan.leclerc-destordeur@student.hepl.be>
2023-12-10 16:38:40 +0100

    10/12

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,44 @@
-const input = document.getElementById("input");
-const list = document.getElementById("list");
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("input");
+  const list = document.getElementById("list");
+  const add = document.getElementById("add");
 
-function addTask() {
-    if (input.value === '') {
-        alert("Tu dois écrire une tache");
+  add.addEventListener("click", addTask);
+
+  list.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("check");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = input.value;
-        list.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+  });
+
+  function addTask() {
+    if (input.value === "") {
+      alert("Tu dois écrire une tâche");
+    } else {
+      let li = document.createElement("li");
+      li.innerHTML = input.value;
+      list.appendChild(li);
+
+      let span = document.createElement("span");
+      span.innerHTML = "\u00d7"; // Caractère "×"
+      li.appendChild(span);
     }
+
     input.value = "";
     saveData();
-}
+  }
 
-list.addEventListener("click", function(e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("check");
-        saveData();
-    }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
-
-function saveData() {
+  function saveData() {
     localStorage.setItem("data", list.innerHTML);
-}
-function showTask() {
+  }
+
+  function showTask() {
     list.innerHTML = localStorage.getItem("data");
-}
-showTask();
+  }
+
+  showTask();
+});
